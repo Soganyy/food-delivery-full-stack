efc27c2670571a7f080d4f0e505a1d55ee6c96dc
Soganyy <matinmammadli0420@gmail.com>
2024-09-13 10:15:39 +0400

    role based auth created

--- wolt-nest-backend/src/item/item.controller.ts
+++ wolt-nest-backend/src/item/item.controller.ts
@@ -13,6 +13,9 @@ import { ItemService } from './item.service';
 import { ItemCreateDto } from './dto/item.create.dto';
 import { AuthGuard } from 'src/auth/guards/jwt.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { RolesGuard } from 'src/auth/guards/role.guard';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import { RolesEnum } from 'src/enums/roles.enum';
 
 @Controller('item')
 export class ItemController {
@@ -26,21 +29,24 @@ export class ItemController {
   }
 
   @Post()
-  @UseGuards(AuthGuard)
+  @Roles(RolesEnum.Merchant)
+  @UseGuards(AuthGuard, RolesGuard)
   @ApiBearerAuth('JWT-auth')
   createItem(@Body() body: ItemCreateDto, @Req() req: any) {
     return this.itemService.createItem(body, req.user);
   }
 
   @Put(':id')
-  @UseGuards(AuthGuard)
+  @Roles(RolesEnum.Merchant)
+  @UseGuards(AuthGuard, RolesGuard)
   @ApiBearerAuth('JWT-auth')
   updateItem(@Body() body: ItemCreateDto, @Param('id') id: number) {
     return this.itemService.updateItem(body, id);
   }
 
   @Delete(':id')
-  @UseGuards(AuthGuard)
+  @Roles(RolesEnum.Merchant)
+  @UseGuards(AuthGuard, RolesGuard)
   @ApiBearerAuth('JWT-auth')
   deleteItem(@Param('id') id: number) {
     return this.itemService.deleteItem(id);
